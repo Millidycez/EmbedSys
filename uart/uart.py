47cef71454c6b7aaf06922fae424a26390a89d75
Patcharapong K <mewzpentium4@gmail.com>
2022-01-31 15:56:29 +0700

    Simple UART coder/decoder

---
+++ uart/uart.py
@@ -0,0 +1,58 @@
+import re
+
+# 30 Jan 2022
+# Simple UART encoder/decoder for Embedded Sys homework.
+
+def bin2hex(bitstr):
+    i = int(bitstr,2)
+    return hex(i)
+
+def bin2char(bitstr):
+    return chr(int(bitstr,2))
+def char2bin(char):
+    return bin(ord(char))[2:].zfill(8)
+
+def decode(bitstr, samplingRate = 4, payloadLength = 8):
+    arr = []
+    # Finding (inside) bits starting with "0000"
+    query = "0" * samplingRate
+    # (inside) payload size 
+    query += "(.{" + str(payloadLength*samplingRate) + "})"
+    bitsArr = re.findall(query,bitstr,re.M)
+
+    # Iterating through each packet
+    for bits in bitsArr:
+        i = payloadLength * samplingRate - 1
+        s = ""
+        while i > 0:
+            s += bits[i] # Get the last elem from "0000" or "1111" Note that this might not work if real world example has noises like "1110", this function will get the "0" out, which is wrong.
+            i -= samplingRate
+        arr.append(bin2char(s))
+    return arr
+
+def encode(charArr, samplingRate = 4, noiseLength = 1):
+    noise = "1" * noiseLength # High value when not sending
+    output = ""
+    output += noise
+    for char in charArr:
+        b = char2bin(char)
+        enc = "0" * samplingRate
+        for bit in b[::-1]:
+            enc += bit * samplingRate
+        enc += "1" * samplingRate
+        output += enc
+        output += noise
+    return output
+    
+# a = decode("10000111100000000000000000000111100001111100000000111100000000000000001111000011111")
+# for item in a:
+#     print(item)
+
+# print(char2bin("P")[::-1])
+
+# b = encode("P")
+# print(b)
+
+# for char in "AB":
+#     print(char2bin(char))
+
